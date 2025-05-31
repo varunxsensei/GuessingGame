adda806ebc4883dcd659495fc180eaf3758f6aab
varunxsensei <varunxai27@gmail.com>
2025-05-31 09:25:09 +0530

    added score count function

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var scoreCount : Int = 0
     
     @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland","Spain","UK","Ukraine","US"].shuffled()
     
@@ -18,14 +19,16 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            Color.blue
+            LinearGradient(colors: [.blue,.black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing:30){
                 VStack{
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 
                 ForEach(0..<3){number in
@@ -33,20 +36,28 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
+                Text("Score \(scoreCount)")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.weight(.semibold))
             }
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue"){
                 askQuestion()
             }
+        } message: {
+            Text("Your score is \(scoreCount)")
         }
         
     }
     func flagTapped(_ number:Int){
         if number == correctAnswer {
             scoreTitle = "Correct"
+            scoreCount += 1
         } else {
             scoreTitle = "wrong"
         }
